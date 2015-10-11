d772f498a86f5482170512bc4837cfc8b3330b11
Cedric Beust <cedric@beust.com>
2015-10-11 12:05:00 -0700

    Build file.

--- Build.kt
+++ Build.kt
@@ -2,33 +2,33 @@ import com.beust.kobalt.*
 import com.beust.kobalt.plugin.packaging.*
 import com.beust.kobalt.plugin.kotlin.*
 import com.beust.kobalt.plugin.publish.*
-//import com.beust.kobalt.plugin.linecount.lineCount
-//
-//val plugins = plugins(
-////    "com.beust.kobalt:kobalt-line-count:0.8"
-////    file(homeDir("kotlin/kobalt-line-count/kobaltBuild/libs/kobalt-line-count-0.8.jar"))
-//    file(homeDir("kotlin/kobalt-line-count/kobaltBuild/libs/kobalt-line-count-0.9.jar"))
-//)
-//
-//val lc = lineCount {
-//    suffix = "**.md"
-//}
+import com.beust.kobalt.plugin.linecount.lineCount
+
+val plugins = plugins(
+//    "com.beust.kobalt:kobalt-line-count:0.8"
+//    file(homeDir("kotlin/kobalt-line-count/kobaltBuild/libs/kobalt-line-count-0.8.jar"))
+    file(homeDir("kotlin/kobalt-line-count/kobaltBuild/libs/kobalt-line-count-0.12.jar"))
+)
+
+val lc = lineCount {
+    suffix = "**.md"
+}
 val project = kotlinProject {
     name = "kobalt-line-count"
     group = "com.beust.kobalt"
     artifactId = name
-    version = "0.10"
+    version = "0.12"
 
     dependencies {
 //        compile("file:" + homeDir("kotlin/kobalt/kobaltBuild/libs/kobalt-0.168.jar"))
-        compile("com.beust:kobalt:0.170")
+        compile("com.beust:kobalt:0.171")
     }
 }
 
 val packProject = assemble(project) {
     mavenJars {
         manifest {
-            attributes("Kobalt-Plugin-Class", "com.beust.kobalt.plugin.linecount.LineCountMain")
+            attributes("Kobalt-Plugin-Class", "com.beust.kobalt.plugin.linecount.LineCountPlugin")
         }
     }
 }
